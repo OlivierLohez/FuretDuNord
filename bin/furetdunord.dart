720418fed2071240cbe7eb65448a73a0a877f61e
Tanguy OZANO <tanguy.ozano@gmail.com>
2022-03-18 11:50:18 +0100

    methode settings

--- bin/furetdunord.dart
+++ bin/furetdunord.dart
@@ -1,4 +1,20 @@
-void main(List<String> arguments) {
-  print('Hello world!');
-  print("Test Tanguy");
+import 'package:mysql1/mysql1.dart';
+
+import 'maBD.dart';
+
+void main(List<String> args) async {
+  ConnectionSettings settings = LaBDFuret().settingsLaBD();
+  try {
+    print("Début");
+    MySqlConnection conn = await MySqlConnection.connect(settings);
+    print("Connexion réussie !");
+    try {
+      await conn.close();
+      print("Connexion terminée !");
+    } catch (e) {
+      print("Erreur de déconnexion !");
+    }
+  } catch (e) {
+    print(e.toString());
+  }
 }
